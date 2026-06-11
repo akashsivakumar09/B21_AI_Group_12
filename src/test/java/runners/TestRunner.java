a3f084b4ca213f4cf492687aad3204259ef4a672
Agash <akashsivakumar09@gmail.com>
2026-06-11 18:48:04 +0530

    adding UI annotations

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions"},
-        tags = "@api and @plant",
+        //tags = "@ui and @category",
         plugin = {
                 "pretty",
                 "html:target/cucumber.html",
